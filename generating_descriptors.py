ea7ae92d9df83fc0de65d12bdffc0fdb9d94a446
Armaan Vakharia <armaanvakharia@gmail.com>
2020-07-16 12:35:53 -0400

    Create generating_descriptors.py

---
+++ generating_descriptors.py
@@ -0,0 +1,19 @@
+# Imports
+import facenet_pytorch as facenet
+import numpy as np
+import mygrad as mg
+
+# Create Rectangles
+def find_faces():
+    """ Uses facenet_pytorch's MTCNN to find faces in given image
+
+    Parameters:
+    -----------
+
+    Returns:
+    --------
+
+    """
+    
+
+# 
