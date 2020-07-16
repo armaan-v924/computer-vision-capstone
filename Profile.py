9dda934c828b452157a02b17672fe01a02230fe8
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-16 14:54:54 -0400

    ok i started the Profile class

---
+++ Profile.py
@@ -0,0 +1,8 @@
+"""Create a Profile class with functionality to add face descriptors and compute the mean descriptor"""
+class Profile: 
+    def __init__(self):
+    """Creates an empty list that contains face descriptors. This list will constantly be updated and saved everytime a new face descriptor was added."""   
+        self.face_list = []
+    
+
+
