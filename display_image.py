6654d887a58db8517177a54a1876cf62bf5d13b2
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-16 16:35:54 -0400

    Updated display_image.py

--- display_image.py
+++ display_image.py
@@ -1,1 +1,13 @@
 """Functionality to display an image with a box around detected faces with labels to indicate matches or an “Unknown” label otherwise"""
+import generating_descriptors as gd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from camera import take_picture
+
+img_array = take_picture()
+descriptors, bounding_boxes, probs = gd.find_faces(img_array)
+img.plot(img_array)
+
+
+
+
