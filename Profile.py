0d02cbf9ec31cab48c8d922b7514838c69d8a803
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-16 14:57:43 -0400

    Fixing invalid syntax error

--- Profile.py
+++ Profile.py
@@ -1,7 +1,7 @@
 """Create a Profile class with functionality to add face descriptors and compute the mean descriptor"""
+"""__init___ = Creates an empty list that contains face descriptors. This list will constantly be updated and saved everytime a new face descriptor was added.""" 
 class Profile: 
-    def __init__(self):
-    """Creates an empty list that contains face descriptors. This list will constantly be updated and saved everytime a new face descriptor was added."""   
+    def __init__(self):  
         self.face_list = []
     
 
