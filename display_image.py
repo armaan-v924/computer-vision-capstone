587849794632695f985d5ac4da501fdba627cf73
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-17 11:55:54 -0400

    i made it into a function

--- display_image.py
+++ display_image.py
@@ -6,16 +6,17 @@ from camera import take_picture
 from matplotlib.patches import Rectangle
 import matchFaces as mf
 import database_functions as df
-img_array = take_picture()
-descriptors, bounding_boxes, probabilities, landmarks = gd.find_faces(img_array)
-fig,ax = plt.subplots()
-ax.imshow(img_array)
-for descriptor, box, prob, _ in zip(descriptors, bounding_boxes, probabilities, landmarks):
-    #draws the box on the plot
-    ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="purple"))
-    label = mf.match_face(descriptor, load_db("database.pkl"),500)
-    #ax.text(box[2:]-box[:2], )
-plt.show()
+def display_image():
+    img_array = take_picture()
+    descriptors, bounding_boxes, probabilities, landmarks = gd.find_faces(img_array)
+    fig,ax = plt.subplots()
+    ax.imshow(img_array)
+    for descriptor, box, prob, _ in zip(descriptors, bounding_boxes, probabilities, landmarks):
+        #draws the box on the plot
+        ax.add_patch(Rectangle(box[:2], *(box[2:] - box[:2]), fill=None, lw=2, color="purple"))
+        label = mf.match_face(descriptor, df.load_db("database.pkl"),500)
+        ax.text(box[:2], *(box[2:] - box[:2]), label, fontsize=15)
+    plt.show()
 
 
 
