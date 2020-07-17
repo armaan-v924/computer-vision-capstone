54565465ddd391eb020a14eb89b950b23a60a8cd
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-16 21:39:46 -0400

    saved

--- display_image.py
+++ display_image.py
@@ -1,13 +1,14 @@
 """Functionality to display an image with a box around detected faces with labels to indicate matches or an “Unknown” label otherwise"""
 import generating_descriptors as gd
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from facenet_models import FacenetModel
 from camera import take_picture
 
 img_array = take_picture()
-descriptors, bounding_boxes, probs = gd.find_faces(img_array)
-imgplot= plt.imgshow(img_array)
-
+model = FacenetModel()
+descriptors, bounding_boxes, probabilities = gd.find_faces(img_array)
+fig,ax = plt.subplots()
+ax.imshow(img_array)
 
 
 
