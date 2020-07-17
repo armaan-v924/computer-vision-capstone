600890a940e2ecaef50c60d8521c22ec181bb9b6
AudreyL713 <2021leea@gmail.com>
2020-07-17 07:16:59 -0700

    attempted version of main.py

---
+++ add_many.py
@@ -0,0 +1,11 @@
+import whisper as wsp
+from pathlib import Path
+
+# Main
+root = Path(".")
+print("Developed by @therealshazam\n")
+print("Please ensure that all of the pictures you would like to add are stored in a single folder.\n")
+path = root / input("Enter the relative file location to your pictures folder: ")
+
+else:
+    print("Sorry something went wrong.")
